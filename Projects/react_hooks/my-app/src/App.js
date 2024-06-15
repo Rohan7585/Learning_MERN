8aa28127d6a5bfd286b46fdfb9f14d4380b0cae3
Rohan <rohan178.rd@gmail.com>
2024-06-15 15:47:17 +0530

    Add Tailwind

--- Projects/react_hooks/my-app/src/App.js
+++ Projects/react_hooks/my-app/src/App.js
@@ -4,10 +4,10 @@ import JokeFetcher from './JokeFetcher';
 function App() {
   return (
     <div className="App">
-      <div>
-      <main>
+      <div class="min-h-screen flex items-center justify-center">
+      <div className='grid grid-cols-2 gap-7 w-[64rem]'>
         <JokeFetcher/>
-      </main>
+      </div>
     </div>
     </div>
   );
--- Projects/react_hooks/my-app/src/JokeFetcher.js
+++ Projects/react_hooks/my-app/src/JokeFetcher.js
@@ -4,23 +4,28 @@ function JokeFetcher() {
     const [joke, setJoke] = useState('');
     const [FetchCount, setFetchCount] = useState(0);
     useEffect(() => {
-        const FetchJoke = async() => {
-            try{
+        const FetchJoke = async () => {
+            try {
                 const response = await fetch('https://api.chucknorris.io/jokes/random');
                 const data = await response.json();
                 setJoke(data.value);
-            }catch(err){
+            } catch (err) {
                 console.log("Error fetching the joke: ", err);
-            }}
-            FetchJoke();
+            }
+        }
+        FetchJoke();
     }, [FetchCount]);
-  return (
-    <div>
-        <h1>This is a random joke application.</h1>
-        <p>{joke}</p>
-        <button onClick = { ()=> setFetchCount(FetchCount + 1)}>New Joke</button>
-    </div>
-  )
+    return (
+        <>
+            <div className="flex justify-center flex-col drop-shadow-2xl items-center border-4 border-black transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+                <h1 className='text-9xl mb-4'>😁</h1>
+                <button className="bg-green-500 rounded-full shadow-md px-6 py-3" onClick={() => setFetchCount(FetchCount + 1)}>New Joke</button>
+            </div>
+            <div className='flex justify-center items-center border-4 border-black drop-shadow-2xl transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300'>
+                <p className='text-center self-center drop-shadow-2xl'>{joke}</p>
+            </div>
+        </>
+    )
 }
 
 export default JokeFetcher
